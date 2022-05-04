f215d600419bec53786121b66afa5fe996b5ff90
Ezequiel_Lazarte <74166838+ezequiel-lazarte@users.noreply.github.com>
2022-05-04 17:11:39 -0300

    se agrego otro lenguaje y se valida el campo mensaje

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-let Buton = document.getElementById("Simulador").disabled = true;
+let buton = document.getElementById("Simulador").disabled = true;
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+});
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    let mensaje = document.getElementById("mensaje").value;
+
+    if(mensaje.length == 0) {
+        alert('No has escrito nada en el cuerpo del mail');
+        return;
+    }
+
+    this.submit();
+}
